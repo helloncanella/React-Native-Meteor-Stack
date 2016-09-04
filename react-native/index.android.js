8c85c98739d01708d4f3e7810c3d691ea9dae612
Hellon Canella <helloncanella@gmail.com>
2016-09-03 21:47:41 -0300

    ok

---
+++ react-native/index.android.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import { AppRegistry } from 'react-native'
+
+
+import App from './app';
+
+class RNApp extends Component {  
+  render() {
+    return <App />;
+  }
+}
+AppRegistry.registerComponent('RNApp', () => RNApp);
